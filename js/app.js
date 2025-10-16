c4c5cb2a3d8ef67c29193ba2946a16c5bb7ef7c1
NataliaMjasnikova <Natalim.Mjasnikova@medieinstitutet.se>
2025-10-16 15:53:54 +0200

    add comments

--- js/app.js
+++ js/app.js
@@ -3,30 +3,27 @@
 const dynamicList = document.getElementById('dynamicList');
 
 
-// Vi väljer alla element som har klassen "clickable-card"
+//* Lägger alla element som har klassen "clickable-card". document.querySelectorAll gör att man kan välja alla tre kort*//
 const clickableCards = document.querySelectorAll('.clickable-card');
 let logCounter = 2;
 
-// Vi lägger till en klicklyssnare på varje enskilt kort i listan
+// Tillägg av klicklyssnare på varje enskilt kort i listan
 clickableCards.forEach(card => {
   card.addEventListener('click', function(event) {
 
     // Här får vi kortets namn från alt-texten eller id.
-    // Genom att använda event.target vet vi exakt vilket kort som klickades på.
+    //  event.target gör så att man vet vilket kort som klickades på.
     const cardName = event.target.alt;
 
-    // 1. Skapa ett nytt <li> element
+    // skapar ett <li> element och lägger till text som kommer upp
     const newListItem = document.createElement('li');
 
-    // 2. Skapa textinnehållet
     const itemText = document.createTextNode(`Kort lagt till: ${logCounter} ${cardName} - ${new Date().toLocaleTimeString()}`);
 
-    // 3. Lägg till textinnehållet i <li> elementet
-    newListItem.appendChild(itemText);
+        newListItem.appendChild(itemText);
 
-    // 4. Modifiera DOM (lägg till element)
     dynamicList.appendChild(newListItem);
-
+   //logcounter räknar hur många gånger hav man klickat på kortet//
     logCounter++;
 
 
